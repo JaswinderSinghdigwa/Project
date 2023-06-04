de58182c6818d16e1ae7a324d68843993577d17f
Jaswinder Singh <findjassi121212@gmail.com>
2023-06-04 19:42:37 +0530

    first Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import { EmployeeCreateComponent } from './components/employee-create/employee-create.component';
+import  {EmployeeEditComponent}  from './components/employee-edit/employee-edit.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,EmployeeEditComponent,EmployeeListComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+   ],
   providers: [],
   bootstrap: [AppComponent]
 })
